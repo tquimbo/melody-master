08c558b719cfbc698870a3f48534da6fece9288b
Tim Quimbo <tim.quimbo@ad.infosys.com>
2023-03-14 09:14:03 -0400

    Initial commit.

---
+++ src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
@@ -0,0 +1,13 @@
+package com.melodymaster.melodymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melodymaster/melodymaster/MelodyMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodymaster.melodymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
