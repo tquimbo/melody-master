04e1bfeb68052ded78634f6e62ea97b052c6018b
Tim Quimbo <tim.quimbo@ad.infosys.com>
2023-03-15 10:39:06 -0400

    Added new AudioFileServiceImpl functions.

--- src/main/java/com/melodymaster/melodymaster/service/AudioFileServiceImpl.java
+++ src/main/java/com/melodymaster/melodymaster/service/AudioFileServiceImpl.java
@@ -1,155 +1,102 @@
-package com.melodymaster.melodymaster.service;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.AudioInputStream;
-
 @Service
-public class AudioFileService {
+public class AudioFileServiceImpl implements AudioFileService {
 
-    @Autowired
-    private NoteRepository noteRepository;
+  @Autowired
+  private NoteRepository noteRepository;
 
-    public void processAudioFile(AudioFileDto audioFileDto) {
-        // Validate the audio file and extract the vocals
-        validateAudioFile(audioFileDto);
-        AudioFileDto vocalsFileDto = extractVocals(audioFileDto);
 
-        // Analyze the vocals and save the pitch, length, and lyrics information
-        List<NoteDto> noteDtos = analyzeVocals(vocalsFileDto);
-        saveNoteDtos(noteDtos);
+
+  private static final double PITCH_THRESHOLD = 0.2; // adjust as needed
+
+  @Override
+  public List<Note> analyzeFile(File audioFile) throws IOException, UnsupportedAudioFileException {
+    // Analyze the audio file and create a list of Note objects
+    List<Note> notes = new ArrayList<>();
+    // ...
+
+    // Save the list of notes to the database
+    List<NoteEntity> noteEntities = new ArrayList<>();
+    for (Note note : notes) {
+      NoteEntity noteEntity = new NoteEntity();
+      noteEntity.setPitch(note.getPitch());
+      noteEntity.setDuration(note.getDuration());
+      noteEntity.setLyrics(note.getLyrics());
+      noteEntities.add(noteEntity);
+    }
+    noteRepository.saveAll(noteEntities);
+
+    return notes;
+  }
+
+  
+  @Override
+  public List<Note> analyzeFile(File audioFile) throws IOException, UnsupportedAudioFileException {
+    List<Note> notes = new ArrayList<>();
+    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+    AudioFormat format = audioInputStream.getFormat();
+    float sampleRate = format.getSampleRate();
+    float frameRate = format.getFrameRate();
+    int channels = format.getChannels();
+    int sampleSizeInBits = format.getSampleSizeInBits();
+    boolean isSigned = format.isSigned();
+    boolean isBigEndian = format.isBigEndian();
+    byte[] audioBytes = audioInputStream.readAllBytes();
+    float[] audioData = AudioFloatConverter.getConverter(format).toFloatArray(audioBytes);
+
+    PitchDetector detector = new Yin(sampleRate, audioData.length);
+    detector.setThreshold(PITCH_THRESHOLD);
+
+    double[] pitches = new double[audioData.length / channels];
+    for (int i = 0; i < audioData.length; i += channels) {
+      float[] frame = new float[channels];
+      for (int j = 0; j < channels; j++) {
+        frame[j] = audioData[i + j];
+      }
+      double pitch = detector.getPitch(frame);
+      pitches[i / channels] = pitch;
     }
 
-    private void validateAudioFile(AudioFileDto audioFileDto) {
-        // Check that the audio file is in a supported format and contains vocals
-        // Throw an exception if the audio file is invalid
-    
-        String filePath = audioFileDto.getFilePath();
-        File file = new File(filePath);
-    
-        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)) {
-            AudioFormat format = audioInputStream.getFormat();
-    
-            if (!format.getEncoding().equals(AudioFormat.Encoding.PCM_SIGNED)) {
-                throw new IllegalArgumentException("Audio file format not supported.");
-            }
-    
-            int numChannels = format.getChannels();
-            int sampleSize = format.getSampleSizeInBits() / 8;
-    
-            if (numChannels < 2 || sampleSize < 2) {
-                throw new IllegalArgumentException("Audio file does not contain vocals.");
-            }
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error validating audio file: " + e.getMessage());
+    int noteStart = 0;
+    double previousPitch = 0;
+    for (int i = 0; i < pitches.length; i++) {
+      if (pitches[i] > 0) {
+        if (previousPitch == 0) {
+          noteStart = i;
         }
-    }
-
-    private AudioFileDto extractVocals(AudioFileDto audioFileDto) {
-        // Use a source separation algorithm to extract the vocals from the audio file
-        // Save the vocals to a new audio file and return the audio file DTO
-    
-        // Here's an example implementation using the TarsosDSP library:
-        try {
-            String filePath = audioFileDto.getFilePath();
-            File file = new File(filePath);
-            AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(file, 2048, 0);
-            AudioProcessor silenceDetector = new SilenceDetector(1, false);
-            AudioProcessor outputWriter = new WriterProcessor(new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 1, 2, 44100, false), new File("vocals.wav"));
-            dispatcher.addAudioProcessor(silenceDetector);
-            dispatcher.addAudioProcessor(outputWriter);
-            dispatcher.run();
-        } catch (Exception e) {
-            throw new RuntimeException("Error extracting vocals from audio file: " + e.getMessage());
+      } else {
+        if (previousPitch > 0) {
+          double pitch = getAveragePitch(pitches, noteStart, i - 1);
+          double duration = (i - noteStart) * frameRate / sampleRate;
+          String lyrics = extractLyrics(audioBytes, noteStart * channels * sampleSizeInBits / 8, i * channels * sampleSizeInBits / 8);
+          Note note = new Note();
+          note.setPitch(pitch);
+          note.setDuration(duration);
+          note.setLyrics(lyrics);
+          notes.add(note);
         }
-    
-        // Return the vocals as a new audio file DTO
-        String vocalsFilePath = "vocals.wav";
-        AudioFileDto vocalsFileDto = new AudioFileDto(vocalsFilePath);
-        return vocalsFileDto;
+      }
+      previousPitch = pitches[i];
     }
 
-    private AudioFileDto extractVocals(AudioFileDto audioFileDto) {
-        // Use a source separation algorithm to extract the vocals from the audio file
-        // Save the vocals to a new audio file and return the audio file DTO
-    
-        // Here's an example implementation using the TarsosDSP library:
-        try {
-            String filePath = audioFileDto.getFilePath();
-            File file = new File(filePath);
-            AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(file, 2048, 0);
-            AudioProcessor silenceDetector = new SilenceDetector(1, false);
-            AudioProcessor outputWriter = new WriterProcessor(new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 1, 2, 44100, false), new File("vocals.wav"));
-            dispatcher.addAudioProcessor(silenceDetector);
-            dispatcher.addAudioProcessor(outputWriter);
-            dispatcher.run();
-        } catch (Exception e) {
-            throw new RuntimeException("Error extracting vocals from audio file: " + e.getMessage());
-        }
-    
-        // Return the vocals as a new audio file DTO
-        String vocalsFilePath = "vocals.wav";
-        AudioFileDto vocalsFileDto = new AudioFileDto(vocalsFilePath);
-        return vocalsFileDto;
-    }
+    return notes;
+  }
 
-    private List<NoteDto> analyzeVocals(AudioFileDto vocalsFileDto) {
-        // Use a pitch analysis algorithm to identify the pitch and length of each note
-        // Use a lyrics recognition algorithm to identify the lyrics of each note
-        // Return a list of note DTOs
-    
-        List<NoteDto> noteDtos = new ArrayList<>();
-    
-        // Here's an example implementation using the TarsosDSP library for pitch analysis:
-        try {
-            String vocalsFilePath = vocalsFileDto.getFilePath();
-            File file = new File(vocalsFilePath);
-            AudioInputStream stream = AudioSystem.getAudioInputStream(file);
-            float sampleRate = stream.getFormat().getSampleRate();
-            float[] samples = new float[stream.available()];
-            int read = stream.read(samples);
-    
-            PitchDetectionHandler pitchHandler = new PitchDetectionHandler() {
-                @Override
-                public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
-                    float pitch = pitchDetectionResult.getPitch();
-                    double timestamp = audioEvent.getTimeStamp();
-                    double pitchConfidence = pitchDetectionResult.getProbability();
-                    // TODO: Use the pitch and timestamp to determine the length of the note
-                    // TODO: Use a lyrics recognition algorithm to determine the lyrics of the note
-                    NoteDto noteDto = new NoteDto(pitch, timestamp, pitchConfidence);
-                    noteDtos.add(noteDto);
-                }
-            };
-    
-            AudioDispatcher dispatcher = new AudioDispatcher(new AudioFloatArrayReader(samples), sampleRate, 2048, 0);
-            AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.YIN, sampleRate, 2048, pitchHandler);
-            dispatcher.addAudioProcessor(pitchProcessor);
-            dispatcher.run();
-        } catch (Exception e) {
-            throw new RuntimeException("Error analyzing vocals: " + e.getMessage());
-        }
-    
-        return noteDtos;
+  private double getAveragePitch(double[] pitches, int startIndex, int endIndex) {
+    double sum = 0;
+    int count = 0;
+    for (int i = startIndex; i <= endIndex; i++) {
+      if (pitches[i] > 0) {
+        sum += pitches[i];
+        count++;
+      }
     }
+    return sum / count;
+  }
 
-    private void saveNoteDtos(List<NoteDto> noteDtos) {
-        // Convert the note DTOs to note entities and save them to the database
-        List<NoteEntity> noteEntities = noteDtos.stream()
-            .map(noteDto -> new NoteEntity(noteDto.getPitch(), noteDto.getLength(), noteDto.getLyrics()))
-            .collect(Collectors.toList());
-        noteRepository.saveAll(noteEntities);
-    }
+  private String extractLyrics(byte[] audioBytes, int startIndex, int endIndex) {
+    // TODO: implement lyrics extraction algorithm
+    return "";
+  }
+
 }
-
-@Repository
-public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
-}
-// In this updated code, @Service annotation indicates that the AudioFileService is a Spring service component. The @Autowired annotation is used to inject an instance of the NoteRepository interface into the service class.
-
-// The @Repository annotation is used to mark the NoteRepository interface as a Spring repository component. This interface extends the JpaRepository interface, which provides CRUD operations for the NoteEntity class.
-
-// Note that this is a simplified example and the actual implementation may differ depending on the specific requirements of the Melody Master app.
-
-
-
-
 
