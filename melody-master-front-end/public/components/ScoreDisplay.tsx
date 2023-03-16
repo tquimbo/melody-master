c8ab0137c124ce4e3b466490217452d7a541b120
Tim Quimbo <tquimbo1@gmail.com>
2023-03-16 01:33:19 -0400

    Added Nav Bar Component. Downloaded Boostrap and Typescript.

---
+++ melody-master-front-end/public/components/ScoreDisplay.tsx
@@ -0,0 +1,31 @@
+// src/components/ScoreDisplay.tsx
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { Container, Row, Col } from 'react-bootstrap';
+
+interface ScoreDisplayProps {
+  userId: string;
+}
+
+const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ userId }) => {
+  const score = useSelector((state: RootState) => state.scores[userId]);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h3>Your Score</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>{score}</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ScoreDisplay;
