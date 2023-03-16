67b67ad925ced4a2a283267f99953c771ac8ab56
Tim Quimbo <tquimbo1@gmail.com>
2023-03-16 01:43:20 -0400

    Added Redux store.

---
+++ melody-master-front-end/src/store/rootReducer.ts
@@ -0,0 +1,12 @@
+// src/store/rootReducer.ts
+
+import { combineReducers } from 'redux';
+// Import your individual reducers here, for example:
+import scoresReducer from './scoresReducer';
+
+const rootReducer = combineReducers({
+  // Add your individual reducers here, for example:
+  scores: scoresReducer,
+});
+
+export default rootReducer;
