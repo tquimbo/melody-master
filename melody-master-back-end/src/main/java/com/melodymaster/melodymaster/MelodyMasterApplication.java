50de371a3427a53fb5c5fd82f0eaea143aa9cd54
Tim Quimbo <tquimbo1@gmail.com>
2023-03-22 20:28:11 -0400

    Added application properties.

--- melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
@@ -1,9 +1,28 @@
+// package com.melodymaster.melodymaster;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// @EntityScan("com.melodymaster.melodymaster.entity")
+// public class MelodyMasterApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(MelodyMasterApplication.class, args);
+//     }
+
+// }
 package com.melodymaster.melodymaster;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.melodymaster.melodymaster.entity")
+@EnableJpaRepositories(basePackages = "com.melodymaster.melodymaster.repository")
 public class MelodyMasterApplication {
 
     public static void main(String[] args) {
