cb21e74d48dd53afb18b72d169f180a6008de3b0
Tim Quimbo <tquimbo1@gmail.com>
2023-03-30 17:38:55 -0400

    Error update.

--- melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
@@ -4,12 +4,12 @@ package com.melodymaster.melodymaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.melodymaster.melodymaster"})
-@EntityScan(basePackages = {"com.melodymaster.melodymaster.entity"})
-@EnableJpaRepositories(basePackages = {"com.melodymaster.melodymaster.repository"})
+@SpringBootApplication
+@EntityScan("com.melodymaster.melodymaster.entity")
+@EnableJpaRepositories("com.melodymaster.melodymaster.repository")
 public class MelodyMasterApplication {
 
     public static void main(String[] args) {
