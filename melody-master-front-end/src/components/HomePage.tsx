951deb43c01fc7ad8ce15c53f952c4771c215b9c
Tim Quimbo <tquimbo1@gmail.com>
2023-04-11 04:19:15 -0400

    Clicking on Melody Master brings you back to home page.

--- melody-master-front-end/src/components/HomePage.tsx
+++ melody-master-front-end/src/components/HomePage.tsx
@@ -15,13 +15,18 @@
 // export default HomePage;
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
 
 const HomePage: React.FC = () => {
   return (
     <Container  style={{ backgroundColor: '#282c34'}}>
       <Row className="justify-content-center mt-5" style={{ color: 'white' }}>
         <Col className="text-center" md={6} style={{ color: 'white' }}>
-          <h1>Melody Master</h1>
+        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+            <h1>Melody Master</h1>
+          </Link>
           <p>Unleash your inner star and master your favorite songs!</p>
          
         </Col>
