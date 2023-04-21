8cbcb6bece7dbf944d08968083b5e05b5a86327e
Tim Quimbo <tquimbo1@gmail.com>
2023-04-21 05:23:51 -0400

    Fixed previous null error and improve Audio and CustomerErrorController classes.

--- melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/MelodyMasterApplication.java
@@ -1,6 +1,8 @@
 
 package com.melodymaster.melodymaster;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +14,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.melodymaster.melodymaster.repository")
 public class MelodyMasterApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MelodyMasterApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting MelodyMasterApplication...");
         SpringApplication.run(MelodyMasterApplication.class, args);
         
     }
--- melody-master-back-end/src/main/java/com/melodymaster/melodymaster/api/AudioAPI.java
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/api/AudioAPI.java
@@ -1,3 +1,5 @@
+package com.melodymaster.melodymaster.api;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.melodymaster.melodymaster.service.AudioProcessingServiceImpl;
@@ -12,18 +14,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RequestMapping("/api/audio")
 public class AudioAPI {
 
-    @Autowired
-
-    private final AudioProcessingServiceImpl AudioProcessingServiceImpl;
+    private final AudioProcessingService audioProcessingService; // Changed variable name to lowercase
 
     @Autowired
-    private final AudioProcessingService AudioProcessingService;
-
-
-    public AudioAPI(AudioProcessingServiceImpl AudioProcessingServiceImpl) {
-        this.AudioProcessingServiceImpl = AudioProcessingServiceImpl;
-        this.AudioProcessingService = AudioProcessingServiceImpl;
+    public AudioAPI(AudioProcessingServiceImpl audioProcessingServiceImpl) {
+        this.audioProcessingService = audioProcessingServiceImpl; // Changed variable name to lowercase
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile audioFile) {
+        try {
+            // Call the audioService to process the audio file
+            audioProcessingService.analyzeFile(audioFile); // Updated variable name to lowercase
+            return ResponseEntity.ok().body("Audio processed successfully");
+        } catch (UnsupportedAudioFileException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
+        }
+    }
+}
+// package com.melodymaster.melodymaster.api;
+
+// import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.multipart.MultipartFile;
+// import com.melodymaster.melodymaster.service.AudioProcessingServiceImpl;
+// import com.melodymaster.melodymaster.service.AudioProcessingService;
+// import org.springframework.http.ResponseEntity;
+// import javax.sound.sampled.UnsupportedAudioFileException;
+// import org.springframework.http.HttpStatus;
+// import java.io.IOException;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// @RestController
+// @RequestMapping("/api/audio")
+// public class AudioAPI {
+
+// //    @Autowired
+
+// //     private final AudioProcessingServiceImpl AudioProcessingServiceImpl;
+ 
+//     private final AudioProcessingService AudioProcessingService;
+
+//     @Autowired
+//     public AudioAPI(AudioProcessingServiceImpl AudioProcessingServiceImpl) {
+//         this.AudioProcessingService = AudioProcessingServiceImpl;
+//     }
+
+//     @PostMapping("/upload")
+//     public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile audioFile) {
+//         try {
+//             // Call the audioService to process the audio file
+//             AudioProcessingService.analyzeFile(audioFile); // Change this line
+//             return ResponseEntity.ok().body("Audio processed successfully");
+//         } catch (UnsupportedAudioFileException e) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
+//         } catch (IOException e) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
+//         }
+//     }
+    
+// }
+
+
     // @PostMapping("/upload")
     // public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile file) {
     //     // Call the audioService to process the audio file
@@ -38,16 +91,3 @@ public class AudioAPI {
 
 //     return ResponseEntity.ok().body("Audio processed successfully");
 // }
-@PostMapping("/upload")
-public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile audioFile) {
-    try {
-        // Call the audioService to process the audio file
-        AudioProcessingServiceImpl.analyzeFile(audioFile);
-        return ResponseEntity.ok().body("Audio processed successfully");
-    } catch (UnsupportedAudioFileException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
-    } catch (IOException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing audio: " + e.getMessage());
-    }
-}
-}
