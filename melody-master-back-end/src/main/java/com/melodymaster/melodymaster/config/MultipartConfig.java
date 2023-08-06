ab2f98f41da1da5ffc7e7cf79219c0a06c790f49
tquimbo <tquimbo1@gmail.com>
2023-08-05 23:19:58 -0400

    Switch app properties to postgres

---
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/config/MultipartConfig.java
@@ -0,0 +1,27 @@
+package com.melodymaster.melodymaster.config;
+
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(20));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(20));
+        return factory.createMultipartConfig();
+    }
+
+}
+
+
+
+
