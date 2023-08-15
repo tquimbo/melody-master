3ee62d62a0a3a1bd84a18098e65ad8eb8a932ec6
tquimbo <tquimbo1@gmail.com>
2023-08-15 00:56:40 -0400

    Added default constructor for Synthesizer.

--- melody-master-back-end/src/main/java/com/melodymaster/melodymaster/entity/Synthesizer.java
+++ melody-master-back-end/src/main/java/com/melodymaster/melodymaster/entity/Synthesizer.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+import javax.persistence.*;
+
 @Entity
 @Table(name = "synthesizer")
 public class Synthesizer {
@@ -24,7 +26,12 @@ public class Synthesizer {
     @Column(name = "model")
     private String model;
 
-    // Constructor
+    // Default constructor
+    public Synthesizer() {
+        // intentionally left blank
+    }
+
+    // Parameterized constructor
     public Synthesizer(String name, String manufacturer, String model) {
         this.name = name;
         this.manufacturer = manufacturer;
@@ -32,6 +39,8 @@ public class Synthesizer {
     }
 
     // Getters and setters
+    // ... [rest of the code remains unchanged]
+        // Getters and setters
     public Long getId() {
         return id;
     }
@@ -64,3 +73,61 @@ public class Synthesizer {
         this.model = model;
     }
 }
+
+// @Entity
+// @Table(name = "synthesizer")
+// public class Synthesizer {
+    
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(name = "name")
+//     private String name;
+
+//     @Column(name = "manufacturer")
+//     private String manufacturer;
+
+//     @Column(name = "model")
+//     private String model;
+
+//     // Constructor
+//     public Synthesizer(String name, String manufacturer, String model) {
+//         this.name = name;
+//         this.manufacturer = manufacturer;
+//         this.model = model;
+//     }
+
+//     // Getters and setters
+//     public Long getId() {
+//         return id;
+//     }
+
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public String getManufacturer() {
+//         return manufacturer;
+//     }
+
+//     public void setManufacturer(String manufacturer) {
+//         this.manufacturer = manufacturer;
+//     }
+
+//     public String getModel() {
+//         return model;
+//     }
+
+//     public void setModel(String model) {
+//         this.model = model;
+//     }
+// }
